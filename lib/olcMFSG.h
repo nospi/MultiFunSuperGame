9fbb1e02a18831572b6c0db6fa55d79111ed9493
nospi <nospi.spam@gmail.com>
2021-01-06 22:26:59 +1100

    added scene infrastructure to olcMFSG

--- lib/olcMFSG.h
+++ lib/olcMFSG.h
@@ -1,5 +1,8 @@
 #pragma once
 #include <olcPixelGameEngine.h>
+#include <vector>
+
+struct scene;
 
 class olcMFSG : public olc::PixelGameEngine
 {
@@ -9,5 +12,11 @@ public:
 	bool OnUserCreate() override;
 	bool OnUserUpdate(float) override;
 	bool OnUserDestroy() override;
+	void LoadScene(const int& id);
+	void LoadScene(const std::string& title);
+	std::vector<scene*>* GetScenes();
+private:
+	std::vector<scene*> scenes;
+	scene* activeScene = nullptr;
+	void setActiveScene(scene*);
 };
-
--- src/olcMFSG.cpp
+++ src/olcMFSG.cpp
@@ -17,10 +17,50 @@ bool olcMFSG::OnUserCreate()
 
 bool olcMFSG::OnUserUpdate(float fElapsedTime)
 {
+	if (activeScene != nullptr)
+		if (!activeScene->Update(fElapsedTime))
+		{
+			if (activeScene->id == 0)
+				return false;
+			activeScene->Destroy();
+			setActiveScene(scenes.front());
+		}
 	return true;
 }
 
 bool olcMFSG::OnUserDestroy()
 {
 	return true;
+}
+
+void olcMFSG::setActiveScene(scene* scn)
+{
+	scn->Create();
+	activeScene = scn;
+	sAppName = "MFSG: " + scn->title;
+}
+
+void olcMFSG::LoadScene(const int& id)
+{
+	auto it = std::find_if(scenes.begin(), scenes.end(), [id](scene* s){ return s->id == id; });
+	if (it != scenes.end())
+	{
+		activeScene->Destroy();
+		setActiveScene(*it);
+	}
+}
+
+void olcMFSG::LoadScene(const std::string& title)
+{
+	auto it = std::find_if(scenes.begin(), scenes.end(), [title](scene* s){ return s->title == title; });
+	if (it != scenes.end())
+	{
+		activeScene->Destroy();
+		setActiveScene(*it);
+	}
+}
+
+std::vector<scene*>* olcMFSG::GetScenes()
+{
+	return &scenes;
 }
