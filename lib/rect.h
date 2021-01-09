eaea2cbb2cd1047ed3db08f8a1364f3cc5e46868
nospi <nospi.spam@gmail.com>
2021-01-10 00:38:14 +1100

    added AABB collision detection to rect

--- lib/rect.h
+++ lib/rect.h
@@ -13,10 +13,30 @@ struct rect
     rect() : position({ 0, 0 }), size({ 1, 1 }) {}
     rect(const T& pos, const T& sz) : position(pos), size(sz) {}
 
+    T topLeft() const
+    {
+        return position;
+    }
+
+    T bottomRight() const
+    {
+        return position + size;
+    }
+
     bool contains(const T& point)
     {
         return position.x <= point.x && point.x <= position.x + size.x &&
             position.y <= point.y && point.y <= position.y + size.y;
+    }
+
+    bool collides(const rect<T>& o)
+    {
+        if (topLeft().x < o.bottomRight().x && bottomRight().x > o.topLeft().x &&
+            topLeft().y < o.bottomRight().y && bottomRight().y > o.topLeft().y)
+        {
+            return true;
+        }
+        return false;
     }
 };
 
